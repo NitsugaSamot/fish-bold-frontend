195fd746e914b421dbd525cf48d8314582c49948
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-12-11 16:14:44 -0300

    ocultando.env

--- components/private-route/PrivateRoute.tsx
+++ components/private-route/PrivateRoute.tsx
@@ -1,34 +1,60 @@
-import { useEffect } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import useAuth from '@/hooks/useAuth';
-import React, { ReactNode } from 'react';
-interface LayoutProps {
+
+interface PrivateRouteProps {
   children: ReactNode;
 }
 
-const PrivateRoute = ({ children }) => {
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const { auth, loading } = useAuth();
   const router = useRouter();
 
-//   useEffect(() => {
-//     if (!loading && !auth._id) {
-//       // Usuario no autenticado, redirigir a la página principal
-//       router.push('/');
-//     }
-//   }, [auth, loading, router]);
-useEffect(() => {
-    if (!auth._id ) {
+  useEffect(() => {
+    if (!auth._id) {
       // Usuario no autenticado, redirigir a la página principal
-    //   router.push('/');
-    <div className="text-white text-center mt-5">
-    <p>Debes iniciar sesión para acceder a esta página.</p>
-  </div>
+      router.push('/');
     }
   }, [auth, loading, router]);
 
-
   // Renderizar children solo si el usuario está autenticado
-  return auth._id ? children : null;
+  return auth._id ? <>{children}</> : null;
 };
 
 export default PrivateRoute;
+
+
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import useAuth from '@/hooks/useAuth';
+// import React, { ReactNode } from 'react';
+// interface LayoutProps {
+//   children: ReactNode;
+// }
+
+// const PrivateRoute = ({ children }) => {
+//   const { auth, loading } = useAuth();
+//   const router = useRouter();
+
+// //   useEffect(() => {
+// //     if (!loading && !auth._id) {
+// //       // Usuario no autenticado, redirigir a la página principal
+// //       router.push('/');
+// //     }
+// //   }, [auth, loading, router]);
+// useEffect(() => {
+//     if (!auth._id ) {
+//       // Usuario no autenticado, redirigir a la página principal
+//     //   router.push('/');
+//     <div className="text-white text-center mt-5">
+//     <p>Debes iniciar sesión para acceder a esta página.</p>
+//   </div>
+//     }
+//   }, [auth, loading, router]);
+
+
+//   // Renderizar children solo si el usuario está autenticado
+//   return auth._id ? children : null;
+// };
+
+// export default PrivateRoute;
