7aa93e1e2249edac59d9d10ad66336609327db83
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-12-11 17:24:47 -0300

    corrigiendo para el deploy otra vez

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -1,68 +1,137 @@
-import { useState, useEffect, createContext } from "react";
+import { useState, useEffect, createContext, ReactNode } from "react";
 import { useRouter } from 'next/router';
 import axiosClient from "@/config/axiosClient";
 
-// const AuthContext = createContext()
+interface AuthProviderProps {
+    children: ReactNode;
+}
 
 const AuthContext = createContext(null);
 
-const AuthProvider = ({children}) => {
-
-    const [auth, setAuth] = useState({})
-    const [loading, setLoading] = useState(true)
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+    const [auth, setAuth] = useState({});
+    const [loading, setLoading] = useState(true);
     const router = useRouter();
 
     useEffect(() => {
         const authenticateUser = async () => {
-            const token = localStorage.getItem('token')
-            if(!token) {
-                setLoading(false)
-                return
+            const token = localStorage.getItem('token');
+            if (!token) {
+                setLoading(false);
+                return;
             }
-    
+
             const config = {
-                headers : {
+                headers: {
                     'Content-Type': 'application/json',
                     Authorization: `Bearer ${token}`
                 }
-            }
-            
+            };
+
             try {
-                const {data} = await axiosClient('/auth/profile', config)
-                setAuth(data)
-                // navigate('/proyectos')
-                router.push('/')
+                const { data } = await axiosClient('/auth/profile', config);
+                setAuth(data);
+                router.push('/');
             } catch (error) {
-                setAuth({})
-            } finally{
-                 setLoading(false) 
+                setAuth({});
+            } finally {
+                setLoading(false);
             }
-    
-            
-        }
-        authenticateUser()
-    }, [])
+        };
+
+        authenticateUser();
+    }, []);
 
     const closeSessionAuth = () => {
         localStorage.removeItem('token');
-        setAuth({})
+        setAuth({});
         console.log("User logged out");
-    }
+    };
 
-  return (
-    <AuthContext.Provider
+    return (
+        <AuthContext.Provider
             value={{
                 auth,
-                setAuth, 
+                setAuth,
                 loading,
                 closeSessionAuth
             }}
-    >
-        {children}
-    </AuthContext.Provider>
-  )
-}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
 
-export { AuthProvider }
+export { AuthProvider };
 
-export default AuthContext
+export default AuthContext;
+
+
+// import { useState, useEffect, createContext } from "react";
+// import { useRouter } from 'next/router';
+// import axiosClient from "@/config/axiosClient";
+
+// // const AuthContext = createContext()
+
+// const AuthContext = createContext(null);
+
+// const AuthProvider = ({children}) => {
+
+//     const [auth, setAuth] = useState({})
+//     const [loading, setLoading] = useState(true)
+//     const router = useRouter();
+
+//     useEffect(() => {
+//         const authenticateUser = async () => {
+//             const token = localStorage.getItem('token')
+//             if(!token) {
+//                 setLoading(false)
+//                 return
+//             }
+    
+//             const config = {
+//                 headers : {
+//                     'Content-Type': 'application/json',
+//                     Authorization: `Bearer ${token}`
+//                 }
+//             }
+            
+//             try {
+//                 const {data} = await axiosClient('/auth/profile', config)
+//                 setAuth(data)
+//                 // navigate('/proyectos')
+//                 router.push('/')
+//             } catch (error) {
+//                 setAuth({})
+//             } finally{
+//                  setLoading(false) 
+//             }
+    
+            
+//         }
+//         authenticateUser()
+//     }, [])
+
+//     const closeSessionAuth = () => {
+//         localStorage.removeItem('token');
+//         setAuth({})
+//         console.log("User logged out");
+//     }
+
+//   return (
+//     <AuthContext.Provider
+//             value={{
+//                 auth,
+//                 setAuth, 
+//                 loading,
+//                 closeSessionAuth
+//             }}
+//     >
+//         {children}
+//     </AuthContext.Provider>
+//   )
+// }
+
+// export { AuthProvider }
+
+// export default AuthContext
