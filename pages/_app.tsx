066850b9f3562d51c5fd94d8b41059cfb8af1180
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-12-11 18:01:50 -0300

    agregando hashrouter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '@/context/AuthProvider'
+import { HashRouter } from 'react-router-dom'
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
+    <HashRouter>
       <AuthProvider>
           <Component {...pageProps} />
       </AuthProvider>
+    </HashRouter>
+
   ) 
 }
