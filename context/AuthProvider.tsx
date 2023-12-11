7415677ab9d7cadb28bd6a6ef76a2bc1fdf488de
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-12-11 17:26:42 -0300

    corrigiendo para el deploy otra vez

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, createContext, ReactNode } from "react";
+import { useState, useEffect, createContext, ReactNode, Dispatch, SetStateAction } from "react";
 import { useRouter } from 'next/router';
 import axiosClient from "@/config/axiosClient";
 
@@ -6,7 +6,14 @@ interface AuthProviderProps {
     children: ReactNode;
 }
 
-const AuthContext = createContext(null);
+interface AuthContextValue {
+    auth: any; // Ajusta según la estructura real de tu objeto auth
+    setAuth: Dispatch<SetStateAction<any>>;
+    loading: boolean;
+    closeSessionAuth: () => void;
+}
+
+const AuthContext = createContext<AuthContextValue | null>(null);
 
 const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const [auth, setAuth] = useState({});
@@ -65,6 +72,75 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 export { AuthProvider };
 
 export default AuthContext;
+
+
+// import { useState, useEffect, createContext, ReactNode } from "react";
+// import { useRouter } from 'next/router';
+// import axiosClient from "@/config/axiosClient";
+
+// interface AuthProviderProps {
+//     children: ReactNode;
+// }
+
+// const AuthContext = createContext(null);
+
+// const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+//     const [auth, setAuth] = useState({});
+//     const [loading, setLoading] = useState(true);
+//     const router = useRouter();
+
+//     useEffect(() => {
+//         const authenticateUser = async () => {
+//             const token = localStorage.getItem('token');
+//             if (!token) {
+//                 setLoading(false);
+//                 return;
+//             }
+
+//             const config = {
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     Authorization: `Bearer ${token}`
+//                 }
+//             };
+
+//             try {
+//                 const { data } = await axiosClient('/auth/profile', config);
+//                 setAuth(data);
+//                 router.push('/');
+//             } catch (error) {
+//                 setAuth({});
+//             } finally {
+//                 setLoading(false);
+//             }
+//         };
+
+//         authenticateUser();
+//     }, []);
+
+//     const closeSessionAuth = () => {
+//         localStorage.removeItem('token');
+//         setAuth({});
+//         console.log("User logged out");
+//     };
+
+//     return (
+//         <AuthContext.Provider
+//             value={{
+//                 auth,
+//                 setAuth,
+//                 loading,
+//                 closeSessionAuth
+//             }}
+//         >
+//             {children}
+//         </AuthContext.Provider>
+//     );
+// };
+
+// export { AuthProvider };
+
+// export default AuthContext;
 
 
 // import { useState, useEffect, createContext } from "react";
